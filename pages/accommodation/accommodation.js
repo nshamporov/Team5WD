b90a68e48ed8bf6f5135adf152dd619e8aaac772
Nikita <nshamporovofficial@gmail.com>
2024-07-07 14:00:28 +0700

    Nikita's comments are done

--- pages/accommodation/accommodation.js
+++ pages/accommodation/accommodation.js
@@ -1,3 +1,4 @@
+//  Dynamic background
 let maxDist;
 let spacing;
 let accommodationsMain;
@@ -8,19 +9,20 @@ let ellipseSize;
 function setup() {
     accommodationsMain = document.querySelector('main');
 
-    accommodationsCanvas = createCanvas(accommodationsMain.offsetWidth, accommodationsMain.offsetHeight);
-    accommodationsCanvas.parent('accommodationsCanvas');
+    accommodationsCanvas = createCanvas(accommodationsMain.offsetWidth, accommodationsMain.offsetHeight);   //  creating canvas with the size of the main tag
+    accommodationsCanvas.parent('accommodationsCanvas');    //  assigning canvas to accommodationsCanvas division by id
 
     maxDist = dist(0, 0, width/2, height/2);
-    spacing = 30;
+    spacing = 30;   // Adjust spacing between ellipses
 }
 
 
 function draw() {
     background(255);
-    fill(255, 217, 217); // SECONDARY LIGHT
+    fill(255, 217, 217);    //  Color of the ellipses
     noStroke();
 
+    //  for loop to draw ellipses
     for (let y = 0; y < height; y += spacing) {
         for (let x = 0; x < width; x += spacing) {
             distance = dist(mouseX, mouseY, x, y);
--- pages/attractions/attractions.js
+++ pages/attractions/attractions.js
@@ -1,27 +1,31 @@
+//  Dynamic background
 let attractionsMain;
 let attractionsCanvas;
 let maxDist;
 let spacing;
+let distance;
+let ellipseSize;
 
 function setup() {
     attractionsMain = document.querySelector('main');
 
-    attractionsCanvas = createCanvas(attractionsMain.offsetWidth, attractionsMain.offsetHeight);
-    attractionsCanvas.parent('attractionsCanvas');
+    attractionsCanvas = createCanvas(attractionsMain.offsetWidth, attractionsMain.offsetHeight);     //  creating canvas with the size of the main tag
+    attractionsCanvas.parent('attractionsCanvas');   //  assigning canvas to attractionsCanvas division by id
 
     maxDist = dist(0, 0, width, height);
-    spacing = 50;
+    spacing = 40;   // Adjust spacing between ellipses
 }
 
 function draw() {
     background(255);
-    fill(255, 217, 217); // SECONDARY LIGHT
+    fill(255, 217, 217);    //  Color of the ellipses
     noStroke();
 
+    //  for loop to draw ellipses
     for (let y = 0; y < height; y += spacing) {
         for (let x = 0; x < width; x += spacing) {
-            let d = dist(mouseX, mouseY, x, y);
-            let ellipseSize = map(d, 0, maxDist, 70, -50);
+            distance = dist(mouseX, mouseY, x, y);
+            ellipseSize = map(distance, 0, maxDist, 60, -50);
             ellipse(x, y, ellipseSize, ellipseSize);
         }
     }
